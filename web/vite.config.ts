c4f392a6c69cdd1628004fe27efb5743f7f7f1bc
claireyy16 <81452928+claireyy16@users.noreply.github.com>
2025-04-07 14:26:27 -0400

    pnpm hates me

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -1,4 +1,4 @@
-import path from "path";
+import { resolve } from "path";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
@@ -8,7 +8,7 @@ export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
+      "@": resolve(__dirname, "./src"),
     },
   },
 });
